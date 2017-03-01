4a09356e27380ee6c4ef79184fbe262d2b384858
Travis Chambers <tjchambers32@gmail.com>
2017-03-01 11:30:47 -0800

    finished naive spread bot

--- bot_spread.py
+++ bot_spread.py
@@ -21,11 +21,11 @@ def make_move(currentBot, currentMap):
 	_bot = currentBot
 	_map = currentMap
 
-	move_outward()
+	make_primary_move()
 
 def make_primary_move():
-	if not move_toward():
-		move_outward()
+	if not move_outward():
+		move_toward()
 
 ######################### Move Outward #########################
 
